e8d1888742fd1a3dfef4fb9125f2617d78676c42
YoruAkio <97880708+YoruAkio@users.noreply.github.com>
2023-11-13 14:31:43 +0000

    init

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+const site = 'https://vo.is-a.dev';
+
+// https://astro.build/config
+export default defineConfig({
+    site,
+    vite: {
+        build: {
+            sourcemap: true,
+        },
+    },
+    markdown: {
+        shikiConfig: {
+            theme: 'css-variables',
+        },
+    },
+    integrations: [
+        mdx(),
+        sitemap({
+            customPages: [site + '/discord', site + '/github'],
+        }),
+        svelte(),
+    ],
+    output: 'server',
+    adapter: cloudflare(),
+});
